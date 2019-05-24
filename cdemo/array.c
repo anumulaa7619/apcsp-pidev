f3cc4840a63cfa24c294decc1baa205e1314a7b7
Arjun Anumula <arjunr2021@gmail.com>
2019-05-24 09:28:07 -0700

    message

--- cdemo/array.c
+++ cdemo/array.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
+
+int arrayAdd(int* arr, int s, int n)
+{
+        for (int i = 0; i < s; i++)
+        {
+        arr[i] = arr[i] + n;
+        printf("%d\n", arr[i]);
+        }
+}
+
 int main()
 {
-	int arrAdd[100] = 
-	
+
+int arr[11];
+
+for (int i = 0; i < 11; i++)
+{
+	arr[i] = i * i;
+}
+
+arrayAdd(arr,11,1);
+
+}
+
+
