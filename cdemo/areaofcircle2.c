44224e9c0c96bcef93d3e7fc793bdc7d3c1ffd6c
anumulaa7619 <50210625+anumulaa7619@users.noreply.github.com>
2019-05-29 20:35:24 -0700

    Update areaofcircle2.c

--- cdemo/areaofcircle2.c
+++ cdemo/areaofcircle2.c
@@ -6,18 +6,19 @@ float areaOfCircle(float r)
   float area = (M_PI * r * r);
   return area;
 }
+//This is an function defined before the int main which returns an area using the accurate value of pi for any given radius
 
 int main()
 {
-  float small, big;
+  float small, big;	//Here we initialize 2 variables where the code will iterate in between
   
   printf("Enter a radius:\n");
-  scanf("%f", &small);
+  scanf("%f", &small);			//take user input
   
   printf("Enter a greater radius:\n");
-  scanf("%f", &big);
+  scanf("%f", &big);			//take user input
   
-  for (float r = small; r <= big; r++)
+  for (float r = small; r <= big; r++)		//Interates through the integers in between the 2 inputted values
   {
 	float compute = areaOfCircle(r);
 	printf("A circle of r = %f is has area = %f\n", radius, compute);
